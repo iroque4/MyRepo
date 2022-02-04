ba327de0c75cf4ba5c1bcab25e1ef4f3faac7e5d
Ivana <iroque1008@gmail.com>
2022-02-03 19:28:32 -0800

    lib + tester

--- MyFile.java
+++ MyFile.java
@@ -1,1 +1,8 @@
-public class MyFile {}
+public class MyFile {
+    public static boolean isSizeEqual(String a, String b) {
+        if(a.length() == b.length()) {
+            return true;
+        }
+        return false;
+    }
+}
